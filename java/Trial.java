3fec44f838b561793e3fe762f4330d878ebf9d78
Humaira Bristy <152265340+Bristy226@users.noreply.github.com>
2025-08-16 14:16:18 +0600

    Add files via upload

---
+++ java/Trial.java
@@ -0,0 +1,28 @@
+
+public class Trial {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+                System.out.println("");
+    }
+    
+    private String name;
+    private int age;
+
+   
+}
+
+
+ 
+public static void trial(name,age){
+   this.name=name;
+   this.age=age;
+    print(name)
+}
+
+
+int y= squre(soto);
+
+trial s1 =new trial(rakib,20)
+
+print(s1.name)
